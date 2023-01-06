de802189e37a7f741de656c2acb1b792778eb3e7
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-06 10:38:08 +0600

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,42 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "icon",
+      href: "/logo.png",
+      type: "image/png",
+    },
+  ]
+}
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Credix - Credit investing made global",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
