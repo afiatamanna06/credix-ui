e9b50798e465ddae0850d0d339bb163963b80338
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-19 19:32:52 +0600

    Add landing section description

--- components/home/LandingSection.tsx
+++ components/home/LandingSection.tsx
@@ -1,11 +1,13 @@
 import { Flex, useColorModeValue } from "@chakra-ui/react";
 import HomeAuroraSection from "./HomeAuroraSection";
+import LandingSectionDescription from "./LandingSectionDescription";
 
 function LandingSection() {
   const bg = useColorModeValue("#f6f6f6", "black");
   const color = useColorModeValue("white", "black");
   return (
-    <Flex bg={bg} color={color}>
+    <Flex bg={bg} direction={["column", "column", "column", "row", "row"]} color={color} py={[24]} justify="space-between">
+      <LandingSectionDescription />
       <HomeAuroraSection />
     </Flex>
   );
--- components/home/LandingSectionDescription.tsx
+++ components/home/LandingSectionDescription.tsx
@@ -1,9 +1,45 @@
-
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import CommonButton from "components/common/CommonButton";
 
 function LandingSectionDescription() {
+  const bg = useColorModeValue("#f6f6f6", "black");
+  const color = useColorModeValue("black", "white");
+
   return (
-    <div>LandingSectionDescription</div>
-  )
+    <Flex direction="column" bg={bg} color={color} gap={6} fontFamily="'IBM Plex Mono', monospace">
+      <Box
+        fontSize={["4xl", "4xl", "6xl", "6xl"]}
+        fontWeight="bold"
+        fontFamily="'Poppins', sans-serif"
+      >
+        Credit investing
+      </Box>
+      <Box
+        fontSize={["4xl", "4xl", "6xl", "6xl"]}
+        fontWeight="bold"
+        mt="-12"
+        fontFamily="'Poppins', sans-serif"
+      >
+        made global
+      </Box>
+      <Box
+        fontSize={["md"]}
+        mt="-6"
+      >
+        Building the future of global credit markets
+      </Box>
+      <Box>
+        <CommonButton
+          bg={color}
+          color={bg}
+          borderColor={color}
+          name="Investment opportunities"
+          hoverBg={bg}
+          hoverColor={color}
+        />
+      </Box>
+    </Flex>
+  );
 }
 
-export default LandingSectionDescription
+export default LandingSectionDescription;
