d40b9631bb5a1d33bccabdc68333924915e95126
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-20 23:38:47 +0600

    Update landing section file

--- components/home/landing-section/LandingSection.tsx
+++ components/home/landing-section/LandingSection.tsx
@@ -6,7 +6,15 @@ function LandingSection() {
   const bg = useColorModeValue("#f6f6f6", "black");
   const color = useColorModeValue("white", "black");
   return (
-    <Flex bg={bg} px={[6, 6, 8, 28, 44]} direction={["column", "column", "column", "row", "row"]} color={color} gap={[8]} py={[8, 8, 20, 24]} justify="space-between">
+    <Flex
+      bg={bg}
+      px={[6, 6, 8, 28, 44]}
+      direction={["column", "column", "column", "row", "row"]}
+      color={color}
+      gap={[8]}
+      py={[8, 8, 20, 24]}
+      justify="space-between"
+    >
       <Box display={["block", "block", "block", "none", "none"]}>
         <HomeAuroraSection />
       </Box>
