6f4970287c1929847a37b9b1bca2e9de06dc047e
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-21 11:17:51 +0600

    Add backed by data mapping

--- components/home/backed-by-section/BackedbySection.tsx
+++ components/home/backed-by-section/BackedbySection.tsx
@@ -1,20 +1,29 @@
 import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import { backedbyData } from "./BackedbyData";
 
 function BackedbySection() {
   const bg = useColorModeValue("white", "#0b0b0b");
-  const color = useColorModeValue("white", "black");
+  const color = useColorModeValue("black", "white");
   return (
     <Flex
       bg={bg}
       px={[6, 6, 8, 28, 44]}
       color={color}
       wrap="wrap"
+      direction={["column", "row", "row", "row"]}
       fontFamily="'IBM Plex Mono', monospace"
       gap={[8]}
-      py={[8, 8, 20, 24]}
+      py={[8, 8, 20, 20]}
     >
-      <Box flex="1">Backed by</Box>
-      
+      <Box flex="1" fontSize="xl" fontWeight="semibold">Backed by</Box>
+      {backedbyData.map(({title, data}) => (
+        <Flex flex="1" key={title} direction="column" gap={3}>
+          <Box fontWeight="semibold">{title}</Box>
+          {data.map((item) => (
+            <Box key={item}>{item}</Box>
+          ))}
+        </Flex>
+      ))}
     </Flex>
   );
 }
