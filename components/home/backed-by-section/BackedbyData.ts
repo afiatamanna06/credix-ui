cc003788a984aed8404a074e1b133aafff94dcc8
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-21 10:58:11 +0600

    Add backed by data

---
+++ components/home/backed-by-section/BackedbyData.ts
@@ -0,0 +1,26 @@
+export const backedbyData = [
+    {
+        title: "Motive Partners",
+        data: [
+            "DRW Cumberland",
+            "Bayhawk",
+            "Transfero Swiss",
+        ],
+    },
+    {
+        title: "ParaFi Capital",
+        data: [
+            "Circle Ventures",
+            "Solana Ventures",
+            "Fuse Capital",
+        ],
+    },
+    {
+        title: "Valor Capital",
+        data: [
+            "Claure Group",
+            "Victory Park Capital",
+            "Abra",
+        ],
+    },
+]
--- components/home/backed-by-section/BackedbySection.tsx
+++ components/home/backed-by-section/BackedbySection.tsx
@@ -1,4 +1,4 @@
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
 
 function BackedbySection() {
   const bg = useColorModeValue("white", "#0b0b0b");
@@ -9,9 +9,12 @@ function BackedbySection() {
       px={[6, 6, 8, 28, 44]}
       color={color}
       wrap="wrap"
+      fontFamily="'IBM Plex Mono', monospace"
       gap={[8]}
       py={[8, 8, 20, 24]}
     >
+      <Box flex="1">Backed by</Box>
+      
     </Flex>
   );
 }
