63f3df9a0ac5dffe215e66852ddf8838a33d27c7
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-25 16:21:20 +0600

    Update core team card file

--- components/home/core-team/CoreTeamCard.tsx
+++ components/home/core-team/CoreTeamCard.tsx
@@ -1,36 +1,48 @@
-import { Box, Flex, Image } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react";
 import { Link } from "@remix-run/react";
 
 interface propTypes {
-    image: string;
-    name: string;
-    position: string;
-    linkedinUrl: string;
-    twitterUrl: string;
+  image: string;
+  name: string;
+  position: string;
+  linkedinUrl: string;
+  twitterUrl: string;
 }
 
-function CoreTeamCard({image, name, position, linkedinUrl, twitterUrl}: propTypes) {
+function CoreTeamCard({
+  image,
+  name,
+  position,
+  linkedinUrl,
+  twitterUrl,
+}: propTypes) {
   return (
-    <Flex direction={["row", "row", "row", "column", "column"]}>
-        <Image src={image} w="5rem" h="8rem" alt="" />
-        <Flex direction="column">
-            <Box>{name}</Box>
-            <Box>{position}</Box>
-            <Flex gap={3}>
-                <Link to={linkedinUrl}>
-                    <Box>
-                        <i className="ri-linkedin-box-fill ri-lg"></i>
-                    </Box>
-                </Link>
-                <Link to={linkedinUrl}>
-                    <Box>
-                        <i className="ri-twitter-fill ri-lg"></i>
-                    </Box>
-                </Link>
-            </Flex>
+    <Flex direction={["row", "row", "row", "column", "column"]} gap={4}>
+      <Image src={image} objectFit="cover" w="10rem" h="13rem" alt="" />
+      <Flex direction="column">
+        <Box
+          fontSize={["md", "md", "lg", "lg"]}
+          fontWeight="bold"
+          fontFamily="'Poppins', sans-serif"
+        >
+          {name}
+        </Box>
+        <Box fontFamily="'IBM Plex Mono', monospace" fontWeight="semibold" fontSize="xs">{position}</Box>
+        <Flex gap={2} pt="4">
+          <Link to={linkedinUrl}>
+            <Box>
+              <i className="ri-linkedin-box-fill ri-lg"></i>
+            </Box>
+          </Link>
+          <Link to={twitterUrl}>
+            <Box>
+              <i className="ri-twitter-fill ri-lg"></i>
+            </Box>
+          </Link>
         </Flex>
+      </Flex>
     </Flex>
-  )
+  );
 }
 
-export default CoreTeamCard
+export default CoreTeamCard;
--- components/home/core-team/CoreTeamSection.tsx
+++ components/home/core-team/CoreTeamSection.tsx
@@ -1,9 +1,11 @@
 import { Flex, useColorModeValue } from "@chakra-ui/react";
+import CoreTeamCard from "./CoreTeamCard";
+import { coreTeamCardData } from "./CoreTeamCardData";
 
 function CoreTeamSection() {
 
   const bg = useColorModeValue("#f6f6f6", "black");
-  const color = useColorModeValue("white", "black");
+  const color = useColorModeValue("black", "white");
   
   return (
     <Flex
@@ -15,7 +17,11 @@ function CoreTeamSection() {
       py={[8, 8, 20, 24]}
       justify="space-between"
     >
-      
+      <Flex direction={["column", "column", "column", "row", "row"]} gap={[4, 4, 4, 12, 14]}>
+        {coreTeamCardData.map(({name, image, position, linkedinUrl, twitterUrl}) => (
+          <CoreTeamCard key={name} name={name} image={image} position ={position} linkedinUrl={linkedinUrl} twitterUrl={twitterUrl} />
+        ))}
+      </Flex>
     </Flex>
   )
 }
