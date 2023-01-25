63ff5c859475c47a2bde849584b0c209c19bdf7e
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-25 15:50:08 +0600

    Update core team card file

--- components/home/core-team/CoreTeamCard.tsx
+++ components/home/core-team/CoreTeamCard.tsx
@@ -1,8 +1,35 @@
+import { Box, Flex, Image } from "@chakra-ui/react"
+import { Link } from "@remix-run/react";
 
+interface propTypes {
+    image: string;
+    name: string;
+    position: string;
+    linkedinUrl: string;
+    twitterUrl: string;
+}
 
-function CoreTeamCard() {
+function CoreTeamCard({image, name, position, linkedinUrl, twitterUrl}: propTypes) {
   return (
-    <div>CoreTeamCard</div>
+    <Flex direction={["row", "row", "row", "column", "column"]}>
+        <Image src={image} w="5rem" h="8rem" alt="" />
+        <Flex direction="column">
+            <Box>{name}</Box>
+            <Box>{position}</Box>
+            <Flex gap={3}>
+                <Link to={linkedinUrl}>
+                    <Box>
+                        <i className="ri-linkedin-box-fill ri-lg"></i>
+                    </Box>
+                </Link>
+                <Link to={linkedinUrl}>
+                    <Box>
+                        <i className="ri-twitter-fill ri-lg"></i>
+                    </Box>
+                </Link>
+            </Flex>
+        </Flex>
+    </Flex>
   )
 }
 
