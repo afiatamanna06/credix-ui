0532a8b9f17a8e3c76727cee9736440fc2d75fff
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-26 09:50:20 +0600

    Add advisors data file

---
+++ components/home/core-team/AdvisorData.ts
@@ -0,0 +1,21 @@
+export const advisorData = [
+    {
+        name: "CHIKE UKAEGBU",
+        position1: "Head crypto",
+        position2: "emerging markets @ VISA"
+    },
+    {
+        name: "JOAO BEZZERRA",
+        position1: "Former managing director &",
+        position2: "CTO @ ITAU"
+    },
+    {
+        name: "REGINALD DE WASSEIGE",
+        position1: "Investor @ Augmentum",
+    },
+    {
+        name: "KENNETH BOK",
+        position1: "MD blocks",
+        position2: "Ex goldman Sachs"
+    },
+]
