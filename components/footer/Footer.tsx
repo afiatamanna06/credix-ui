deba7df3920220d9f9f351a17ab87c83a7ad03c2
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-29 13:13:01 +0600

    Update footer file

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,22 @@
-
+import { Flex, useColorModeValue } from "@chakra-ui/react";
 
 function Footer() {
+  const bg = useColorModeValue("#f6f6f6", "black");
+  const color = useColorModeValue("black", "white");
+
   return (
-    <div>Footer</div>
-  )
+    <Flex
+      bg={bg}
+      px={[6, 6, 8, 28, 44]}
+      direction={["column", "column", "column", "row", "row"]}
+      color={color}
+      gap={[28, 28, 8, 8]}
+      py={[8, 8, 20, 24]}
+      justify="space-between"
+    >
+        
+    </Flex>
+  );
 }
 
-export default Footer
+export default Footer;
