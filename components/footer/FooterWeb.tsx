a99d4776ad1c30bc5824ed3b99bd3ce878ced0b7
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-29 14:43:07 +0600

    Add bottom footer links mapping

--- components/footer/FooterWeb.tsx
+++ components/footer/FooterWeb.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Image, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Link } from "@remix-run/react";
+import { bottomFooterLinks } from "./FooterLinks";
 
 function FooterWeb() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -7,8 +15,16 @@ function FooterWeb() {
   const color = useColorModeValue("black", "white");
 
   return (
-    <Flex direction={["column", "column", "column", "row", "row"]} border="1px" w="full" bg={bg} color={color} display={["none", "none", "none", "flex"]}>
-      <Flex direction="column" justify="space-between" p="5">
+    <Flex
+      direction={["column", "column", "column", "row", "row"]}
+      border="1px"
+      w="full"
+      fontFamily="'IBM Plex Mono', monospace"
+      bg={bg}
+      color={color}
+      display={["none", "none", "none", "flex"]}
+    >
+      <Flex direction="column" justify="space-between" p="5" gap={6}>
         <Box>
           {colorMode === "light" ? (
             <Image src="/logo_black.png" h={["4rem"]} w="3rem" alt="" />
@@ -16,6 +32,19 @@ function FooterWeb() {
             <Image src="/logo_white.png" h={["4rem"]} w="3rem" alt="" />
           )}
         </Box>
+        <Flex gap={2}>
+          {bottomFooterLinks.map(({ path, name, target }) => (
+            <Box key={name} fontSize="xs">
+              {target ? (
+                <a href={path} target="_blank" rel="noreferrer">
+                  {name}
+                </a>
+              ) : (
+                <Link to={path}>{name}</Link>
+              )}
+            </Box>
+          ))}
+        </Flex>
       </Flex>
     </Flex>
   );
