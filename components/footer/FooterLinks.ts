63a235939fe88f5596a72c126bb5d23467fe3402
afiatamanna06 <afiatamanna06@gmail.com>
2023-01-29 13:32:55 +0600

    Add footer links file

---
+++ components/footer/FooterLinks.ts
@@ -0,0 +1,27 @@
+export const footerLinks = [
+  {
+    path: "/investors",
+    name: "Investors",
+    target: false,
+  },
+  {
+    path: "/borrowers",
+    name: "Borrowers",
+    target: false,
+  },
+  {
+    path: "https://docs.credix.finance/",
+    name: "Documentation",
+    target: true,
+  },
+  {
+    path: "https://angel.co/company/credix",
+    name: "Careers",
+    target: true,
+  },
+  {
+    path: "https://status.credix.finance/",
+    name: "Status",
+    target: true,
+  },
+];
