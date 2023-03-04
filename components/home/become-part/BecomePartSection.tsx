1ea42639cae55a7381020d58524d1c9523b8f22f
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-04 16:45:32 +0600

    Add become part section buttons

--- components/home/become-part/BecomePartSection.tsx
+++ components/home/become-part/BecomePartSection.tsx
@@ -19,10 +19,29 @@ function BecomePartSection() {
         fontSize={["2xl", "2xl", "3xl", "4xl"]}
         fontWeight="450"
         fontFamily="'Poppins', sans-serif"
+        lineHeight={1}
         maxWidth={["100%", "100%", "100%", "30rem"]}
       >
         Become part of the future of private credit markets
       </Box>
+      <Flex gap={6} mt={6} direction={["column", "column", "column", "row", "row"]}>
+        <CommonButton
+          bg={bg}
+          color={color}
+          borderColor={color}
+          name="Onboard as an investor"
+          hoverBg={color}
+          hoverColor={bg}
+        />
+        <CommonButton
+          bg={color}
+          color={bg}
+          borderColor={color}
+          name="Register as a borrower"
+          hoverBg={bg}
+          hoverColor={color}
+        />
+      </Flex>
     </Flex>
   );
 }
