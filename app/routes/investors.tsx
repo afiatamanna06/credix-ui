64a80eee3bd38b6cd22976ae08b1e8e2418de281
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-09 13:03:49 +0600

    Add investors landing section description

--- app/routes/investors.tsx
+++ app/routes/investors.tsx
@@ -1,11 +1,12 @@
 import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import LandingSection from "components/investors/landing-section/LandingSection";
 
 const Investors = () => {
   const bg = useColorModeValue("#f6f6f6", "black");
   const color = useColorModeValue("white", "black");
   return (
     <Flex direction="column" minH="100vh" bg={bg} color={color}>
-      <Box>Investors</Box>
+      <LandingSection />
     </Flex>
   );
 };
--- components/investors/landing-section/LandingSectionDescription.tsx
+++ components/investors/landing-section/LandingSectionDescription.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import CommonButton from "components/common/CommonButton";
 
-const LandingSectionDescription = () => {
+function LandingSectionDescription() {
+  const bg = useColorModeValue("#f6f6f6", "black");
+  const color = useColorModeValue("black", "white");
+
   return (
-    <div>LandingSectionDescription</div>
-  )
+    <Flex
+      direction="column"
+      bg={bg}
+      color={color}
+      gap={6}
+      fontFamily="'IBM Plex Mono', monospace"
+    >
+      <Box
+        fontSize={["4xl", "4xl", "6xl", "7xl"]}
+        fontWeight="bold"
+        fontFamily="'Poppins', sans-serif"
+      >
+        Invest
+      </Box>
+      <Box
+        fontSize={["4xl", "4xl", "6xl", "7xl"]}
+        fontWeight="bold"
+        mt={["-4", "-4", "-4", "-12"]}
+        fontFamily="'Poppins', sans-serif"
+      >
+        With Credix
+      </Box>
+      <Box fontSize={["md"]} mt={[0, 0, 0, "-6"]} maxW="30rem">
+        Private debt investments with sustainable, non-crypto correlated returns, backed by real-world assets
+      </Box>
+      <Box>
+        <CommonButton
+          bg={color}
+          color={bg}
+          borderColor={color}
+          name="Onboard as an investor"
+          hoverBg={bg}
+          hoverColor={color}
+        />
+      </Box>
+    </Flex>
+  );
 }
 
-export default LandingSectionDescription
+export default LandingSectionDescription;
