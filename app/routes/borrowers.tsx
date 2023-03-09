175516f4dfe9701b6257229230e2b2c8e1f58f89
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-09 12:29:15 +0600

    Add investors and borrowers page

--- app/routes/borrowers.tsx
+++ app/routes/borrowers.tsx
@@ -1,13 +1,13 @@
 import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
 
-const Investors = () => {
+const Borrowers = () => {
   const bg = useColorModeValue("#f6f6f6", "black");
   const color = useColorModeValue("white", "black");
   return (
     <Flex direction="column" minH="100vh" bg={bg} color={color}>
-      <Box>Investors</Box>
+      <Box>Borrowers</Box>
     </Flex>
   );
 };
 
-export default Investors;
+export default Borrowers;
