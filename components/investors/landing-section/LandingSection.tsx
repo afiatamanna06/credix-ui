a9ef0a99d27d1df002ac10b8323de3a4efebfce5
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-09 12:58:54 +0600

    Add investors landing section

---
+++ components/investors/landing-section/LandingSection.tsx
@@ -0,0 +1,22 @@
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import LandingSectionDescription from "./LandingSectionDescription";
+
+function LandingSection() {
+  const bg = useColorModeValue("#f6f6f6", "black");
+  const color = useColorModeValue("white", "black");
+  return (
+    <Flex
+      bg={bg}
+      px={[6, 6, 8, 28, 44]}
+      direction={["column", "column", "column", "row", "row"]}
+      color={color}
+      gap={[8]}
+      py={[8, 8, 20, 24]}
+      justify="space-between"
+    >
+      <LandingSectionDescription />
+    </Flex>
+  );
+}
+
+export default LandingSection;
---
+++ components/investors/landing-section/LandingSectionDescription.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingSectionDescription = () => {
+  return (
+    <div>LandingSectionDescription</div>
+  )
+}
+
+export default LandingSectionDescription
