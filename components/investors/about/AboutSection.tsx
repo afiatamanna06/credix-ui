4b03a655d603c2e592ecbe974e6864757a8bee08
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-12 11:40:44 +0600

    Add about buttons and links

--- components/investors/about/AboutSection.tsx
+++ components/investors/about/AboutSection.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import CommonButton from "components/common/CommonButton";
 import CommonCard from "components/common/CommonCard";
 import { aboutData } from "./AboutSectionData";
 
@@ -11,7 +12,7 @@ const AboutSection = () => {
       px={[6, 6, 8, 28, 44]}
       color={color}
       wrap="wrap"
-      direction={["column", "row", "row", "row"]}
+      direction={["column"]}
       fontFamily="'IBM Plex Mono', monospace"
       gap={[8]}
       w="full"
@@ -23,9 +24,29 @@ const AboutSection = () => {
         w="full"
       >
         {aboutData.map(({ title, data }) => (
-          <CommonCard key={title} title={title} data={data} border="#0046fa" borderHeight=".50rem" />
+          <CommonCard
+            key={title}
+            title={title}
+            data={data}
+            border="#0046fa"
+            borderHeight=".50rem"
+          />
         ))}
       </Flex>
+      <Box mt="12">
+        <CommonButton
+          bg={color}
+          color={bg}
+          borderColor={color}
+          name="Launch App"
+          hoverBg={bg}
+          hoverColor={color}
+          width={["full", "full", "full", "17rem"]}
+        />
+      </Box>
+      <a href={"https://docs.credix.finance/"} target="_blank" rel="noreferrer">
+        <Flex direction="column" textDecoration="underline">{`Read our documentation ->`}</Flex>
+      </a>
     </Flex>
   );
 };
