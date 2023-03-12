4980cee104fa89484554f5f358f8ecf5377a9b4a
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-12 15:09:26 +0600

    Add borrowers landing section file

---
+++ components/borrowers/landing-section/LandingSection.tsx
@@ -0,0 +1,21 @@
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+
+function LandingSection() {
+  const bg = useColorModeValue("#f6f6f6", "black");
+  const color = useColorModeValue("white", "black");
+  return (
+    <Flex
+      bg={bg}
+      px={[6, 6, 8, 28, 44]}
+      direction={["column", "column", "column", "row", "row"]}
+      color={color}
+      gap={[8]}
+      py={[8, 8, 20, 24]}
+      justify="space-between"
+    >
+      
+    </Flex>
+  );
+}
+
+export default LandingSection;
