938fe7f1bb9e479319a058ac99f6e0469b834cea
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-12 18:58:54 +0600

    Add borrowers about section data

---
+++ components/borrowers/about/AboutSectionData.ts
@@ -0,0 +1,14 @@
+export const aboutData = [
+    {
+        title: "DEFI 2.0",
+        data: "Our digital native platform lets you access capital within weeks."
+    },
+    {
+        title: "Save on costs",
+        data: "Save up to 300bps depending on the deal size."
+    },
+    {
+        title: "Access untapped liquidity",
+        data: "Get access to institutional capital via our ecosystem of global investors."
+    },
+]
