01ad1088e67d3c48e40a6d7b7ae6726227a2484b
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-13 12:20:01 +0600

    Add investors section data

---
+++ components/investors/investors-name/InvestorsImages.ts
@@ -0,0 +1,23 @@
+export const coreTeamCardData = [
+  {
+    image: "/parafi.webp",
+  },
+  {
+    image: "/cumberland-logo.webp",
+  },
+  {
+    image: "/mgnr.webp",
+  },
+  {
+    image: "/rockaway.webp",
+  },
+  {
+    image: "/arca.webp",
+  },
+  {
+    image: "/fuse.webp",
+  },
+  {
+    image: "/mudrex.webp",
+  },
+];
