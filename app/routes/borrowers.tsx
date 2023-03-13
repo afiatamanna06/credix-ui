ac4e5728f0b00f4675eb6ab55cb6f9c69436c988
afiatamanna06 <afiatamanna06@gmail.com>
2023-03-13 09:43:58 +0600

    Add common become part to borrowers page

--- app/routes/borrowers.tsx
+++ app/routes/borrowers.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
 import AboutSection from "components/borrowers/about/AboutSection";
 import LandingSection from "components/borrowers/landing-section/LandingSection";
+import CommonBecomePart from "components/common/CommonBecomePart";
 
 const Borrowers = () => {
   const bg = useColorModeValue("#f6f6f6", "black");
@@ -9,6 +10,13 @@ const Borrowers = () => {
     <Flex direction="column" minH="100vh" bg={bg} color={color}>
       <LandingSection />
       <AboutSection />
+      <CommonBecomePart
+        title="Be part of the journey"
+        buttonText="Register as a borrower"
+        border="#f8bb00"
+        borderHeight=".25rem"
+        borderWidth={"5rem"}
+      />
     </Flex>
   );
 };
