252f499fb4cb8d49b4b799607a8d810181a041d7
afiatamanna06 <afiatamanna06@gmail.com>
2023-04-01 20:43:52 +0600

    Update home about section data

---
+++ components/home/about-section/AboutSectionData.ts
@@ -0,0 +1,19 @@
+export const aboutSectionData = [
+    {
+        heading: "Vision & Mission",
+        subHeading: "Building the future of global credit markets",
+        description: "Connecting institutional investors globally with credit opportunities - Unlocking risk-adjusted returns through robust underwriting - Building on the most advanced decentralized technologies to achieve scale"
+    },
+    {
+        heading: "For Investors",
+        description: "Institutional investors like hedge funds, family offices, and alternative asset managers are accessing Credix to enhance returns and diversify risk.",
+        link: "in",
+        buttonText: "Onboard as an investor"
+    },
+    {
+        heading: "For Borrowers",
+        description: "Credit fintechs and non-bank loan originators in emerging markets can raise debt funding internationally with less friction and at attractive rates.",
+        link: "br",
+        buttonText: "Register as a borrower"
+    },
+]
